5cf8758db9ad9e8ca81d1891f984f0eca19c69ec
xiaoyao21 <1395327616@qq.com>
2019-05-25 18:27:58 +0800

    切换分支 修改商品信息

--- vegetable-managenment/src/components/addgoods.vue
+++ vegetable-managenment/src/components/addgoods.vue
@@ -1,32 +1,61 @@
 <template>
 	<div class="all">
 		<p v-if="number==undefined" class="title">添加新商品</p>
-		<p v-else>jiayou </p>
+		<p v-else class="title">在线商品管理<img src="../assets/箭头.png"><span>编辑</span></p>
+		<div class="content">
+			<div class="left">
+				<span>商品名称：</span><input type="text" value=""><br><br>
+				<span>商品简介：</span>
+				<textarea></textarea><br><br><br><br><br><br>
+				<span>添加图片：</span>
+				<div>
+					<span>+</span>
+				</div><br>
+				
+				<span>添加视频：</span>
+				<div>
+					<span>+</span>
+				</div><br>
+			</div>
 
-		<div class="left">
-			<span>商品名称：</span><input type="text" value=""><br>
-			<span>商品简介：</span><input type="textarea" value=""><br>
-			<span>添加图片：</span>
-			<div><span>+</span></div><br>
-			<span>添加视频：</span>
-			<div><span>+</span></div><br>
+			<div class="mid">
+				<span class="dis-left">单位（元）：</span><input type="text" value="" class="dis-right"><br><br><br>
+				<span class="dis-left">会员价（元）：</span><input type="text" value="" class="dis-right"><br><br><br>
+				<span class="dis-left">商品分类：</span>
+				<select name="sel" class="dis-right">
+					<option value="1">蔬菜</option>
+					<option value="2">水果</option>
+					<option value="3">海鲜</option>
+					<option value="4">肉蛋</option>
+				</select><br><br><br>
+				<span class="dis-left">供应商：</span><input type="text" value="" class="dis-right" placeholder="请选择商品供应商"><br><br><br>
+				<span class="dis-left">库存：</span><input type="text" value="" class="dis-right" placeholder="请输入商品库存量"><br><br><br>
+			</div>
+
+			<div class="right">
+
+				<template v-for="(name,index) in left.name">
+					<span class="dit-left">{{name}}：</span><input type="text" value="" class="dit-right" v-bind:placeholder="left.top[index]"><br><br><br>
+				</template>
+
+				<span class="dit-left">设置：</span>
+				<img src="../assets/未勾选.png" @click="one=!one" v-if="one" />
+				<img src="../assets/勾选.png" @click="one=!one" v-else />
+
+				<span>是否加入促销（元）</span><br><br>
+				<input type="text" value="" class="dit-right" placeholder="请输入报销价"><br><br>
+				<img src="../assets/未勾选.png" @click="two=!two" v-if="two" />
+				<img src="../assets/勾选.png" @click="two=!two" v-else />
+
+				<span>是否加入积分兑换商品（积分）</span><br><br>
+				<input type="text" value="" placeholder="请输入兑换积分" class="dit-right"><br><br>
+			</div>
 		</div>
 
-		<div class="mid">
-			<span>单位（元）：</span><input type="text" value="">
-			<span>会员价（元）：</span><input type="text" value="">
-			<span>商品分类：</span>
-			<select name="sel">
-				<option value="1">蔬菜</option>
-				<option value="2">水果</option>
-				<option value="3">海鲜</option>
-				<option value="4">肉蛋</option>
-			</select>
-			<span>供应商：</span><input type="text" value="">
-			<span>库存：</span><input type="text" value="">
+		<div>
+			<button class="backd">取消</button>
+			<button class="opend">确定</button>
 		</div>
-
-		<div class="right"></div>
 	</div>
 
 </template>
@@ -35,53 +64,33 @@
 		name: "addgoods",
 		data() {
 			return {
-				number: undefined
+				number: undefined,
+				one: true,
+				two: true,
+				left: {
+					name: ["产地", "净含量", "保质期", "保存条件"],
+					top: ["请输入商品产地", "请输入商品净含量", "请输入商品保质期", "请输入商品保存条件"],
+
+				},
 			}
 		},
-		mounted() {
-			console.log(this.$route.query.num)
-			this.number = this.$route.query.num;
-		}
+		methods: {
 
+		},
+		mounted() {
+			this.number = this.$route.query.num;
+		},
+		watch: {
+			$route: {
+				handler: function() {
+					this.number = undefined
+				}
+			}
+		}
 	}
 </script>
 
 
 <style scoped>
-	.all span {
-		font-family: MicrosoftYaHei;
-		font-size: 18px;
-		color: #222222;
-		font-weight: 900;
-	}
-
-	.title {
-		font-family: MicrosoftYaHei;
-		font-size: 20px;
-		color: #222222;
-		margin-left: 50px;
-	}
-
-	.left {
-		width: 460px;
-		height: 630px;
-		background-color: #1CB906;
-	}
-
-	.left input :nth-child(1){
-		width: 362px;
-		height: 35px;
-		background: #F6F6F6;
-		border-radius: 4px;
-	}
-
-	.mid {
-		width: ;
-		height: ;
-	}
-
-	.right {
-		width: ;
-		height: ;
-	}
+	@import '../css/addgoods.css';
 </style>
