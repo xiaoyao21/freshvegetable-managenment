0b59e72eb8c492cfa361f0cd94c485346e5c693f
xiaoyao21 <1395327616@qq.com>
2019-05-30 17:54:54 +0800

    翻页组件完成

--- vegetable-managenment/src/components/change_pages.vue
+++ vegetable-managenment/src/components/change_pages.vue
@@ -1,33 +1,30 @@
 <template>
 	<div id="change">
-		<ul @click="delegate()" ref="ulul">
-			<span @click="page_left()" :class="style">上一页</span>
-
+		<ul ref="ulul">
 			<li v-for="(page,index) in pages" v-if="pages<=5">
 				{{page}}
 			</li>
 
-			<template v-if="pages>5" >
-				<li >1</li>
+			<span @click="page_left(2)" :class=style>上一页</span>
+
+			<template v-if="pages>5">
+				<li @click="start()" class="sty">1</li>
 				<span v-show="show_left">...</span>
-				<li >2</li>
-				<li >3</li>
-				<li >4</li>
+				<li @click="delegate(),page_left(1)">2</li>
+				<li @click="delegate()">3</li>
+				<li @click="delegate(),page_right(1)">4</li>
 				<span v-show="show_right">...</span>
-				<li >{{pages}}</li>
+				<li @click="end()">{{pages}}</li>
 			</template>
 
-			<span @click="page_right()">下一页</span>
-
+			<span @click="page_right(2)" :class=style1>下一页</span>
 		</ul>
-
 	</div>
 </template>
 
 <script>
 	export default {
 		name: 'change',
-
 		data() {
 			return {
 				pages: null,
@@ -35,66 +32,150 @@
 				show_right: true,
 				li: null,
 				style: {
-					one: !this.show_left,
-					two: this.show_left
+					one: true,
+					two: false,
 				},
-				currentPage:1,
-				
+				style1: {
+					one1: false,
+					two1: true,
+				},
+				currentPage: 1,
+				now: 1
 			}
 		},
 		methods: {
 			delegate() {
 				var e = e || window.event;
 				var target = e.target || e.srcElement;
-				this.currentPage = target.innerHTML;
-				console.log(this.currentPage)
-				// console.log(this.li[this.currentPage-1])
 
-			},
-			page_left() {
-				if(this.currentPage>1)
-				{
-						this.currentPage--;
+				if (this.currentPage != "...") {
+					this.currentPage = target.innerHTML;
 				}
-				if (this.li[1].innerHTML > 2) {
+			},
+			page_left(haha) {
+				if (haha == 2)
+					if (this.currentPage > 1) {
+						this.currentPage--;
+					}
+
+				if (this.currentPage >= 3 && this.pages - this.currentPage >= 3) {
 					this.li[1].innerHTML--;
 					this.li[2].innerHTML--;
 					this.li[3].innerHTML--;
 				}
-
 				if (this.pages - this.li[3].innerHTML > 1) {
 
 					this.show_right = true
 				}
-				if (this.li[1].innerHTML - 1 <= 1) {
+				if (this.currentPage == 3) {
 					this.show_left = false
 				}
-			},
-			page_right() {
-				if(this.currentPage < 15){
-					this.currentPage++;
+				if (this.currentPage == 1) {
+					this.style.one = true;
+					this.style.two = false;
+				} else {
+					this.style.one = false;
+					this.style.two = true;
 				}
-				
-				if (this.li[3].innerHTML < this.pages - 1) {
+
+				if (this.currentPage == this.pages) {
+					this.style1.one1 = true;
+					this.style1.two1 = false;
+
+
+				} else {
+					this.style1.one1 = false;
+					this.style1.two1 = true;
+				}
+
+			},
+			page_right(haha) {
+
+
+				if (haha == 2) {
+					if (this.currentPage < this.pages) {
+						this.currentPage++;
+					}
+				}
+
+				if (this.currentPage >= 4 && this.pages - this.currentPage >= 2) {
 					this.li[1].innerHTML++;
 					this.li[2].innerHTML++;
 					this.li[3].innerHTML++;
-
 				}
 				if (this.li[1].innerHTML - 1 > 1) {
 					this.show_left = true
-				}
-				if (this.pages - this.li[3].innerHTML <= 1) {
 
+				}
+				if (this.currentPage == this.pages - 2) {
 					this.show_right = false
 				}
+				if (this.currentPage == this.pages) {
+					this.style1.one1 = true;
+					this.style1.two1 = false;
+
+
+				} else {
+					this.style1.one1 = false;
+					this.style1.two1 = true;
+				}
+
+				if (this.currentPage == 1) {
+					this.style.one = true;
+					this.style.two = false;
+				} else {
+					this.style.one = false;
+					this.style.two = true;
+				}
+
+			},
+			start() {
+				this.delegate()
+				this.li[1].innerHTML = "2";
+				this.li[2].innerHTML = "3";
+				this.li[3].innerHTML = "4";
+
+				this.style1.one1 = false;
+				this.style1.two1 = true;
+				this.style.one = true;
+				this.style.two = false;
+
+				this.show_left = false
+				this.show_right = true;
+			},
+			end() {
+				this.delegate()
+				this.li[1].innerHTML = this.pages - 3;
+				this.li[2].innerHTML = this.pages - 2;
+				this.li[3].innerHTML = this.pages - 1;
+
+				this.style1.one1 = true;
+				this.style1.two1 = false;
+				this.style.one = false;
+				this.style.two = true;
+
+				this.show_right = false;
+				this.show_left = true;
 			}
 
 		},
+
 		mounted() {
 			this.pages = 15;
-			var currentPage = 1;
+			this.currentPage = 1;
 			this.li = this.$refs.ulul.getElementsByTagName("li")
+		},
+		watch: {
+			currentPage: function(val) {
+				for (var i = 0; i < 5; i++) {
+					if (this.li[i].innerHTML == val) {
+						//console.log(this.li[i])
+						this.li[i].className = "sty"
+					} else {
+						this.li[i].className = ""
+					}
+				}
+			}
 		}
 	}
 </script>
@@ -105,7 +186,6 @@
 		height: 38px;
 		line-height: 38px;
 		text-align: center;
-		/* background-color: #c6c6c6; */
 		display: inline-block;
 		cursor: pointer;
 	}
@@ -118,13 +198,19 @@
 		cursor: pointer;
 	}
 
-	.two {
+	.one,
+	.one1 {
 		cursor: default;
 		background-color: #fff;
 		color: red;
 	}
-	
-	
 
+	.two,
+	.two1 {
+		color: #222222;
+	}
 
+	.sty {
+		background-color: #19A605;
+	}
 </style>
